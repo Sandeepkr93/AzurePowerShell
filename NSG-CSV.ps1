99cc0351279e3418c39f3befd412a8c65c94754e
Sandeepkr93 <58113378+Sandeepkr93@users.noreply.github.com>
2020-06-03 15:22:42 +0530

    Export NSG to CSV

---
+++ NSG-CSV.ps1
@@ -0,0 +1,51 @@
+﻿#Connect-AzAccount
+$subs = Get-AzSubscription 
+$output1 = @();
+$exportPath = 'C:\Users\M1056612\Desktop\Azure Network Overivew ARM\NSG.csv'
+$portNum = Read-Host -Prompt "Enter the Port Number"
+try
+{
+    foreach ($sub in $subs)
+    {
+    #$null = $PSItem | Select-AzSubscription
+    Set-AzContext -Subscription $sub | Out-Null
+    $nsgs = Get-AzNetworkSecurityGroup 
+    if($portNum)
+    {
+            foreach ($obj in $nsgs)
+            {
+               #$customPsObject = New-Object -TypeName PsObject
+                $output1 += $obj.SecurityRules | Where-Object { $_.DestinationPortRange -eq $portNum} | Select-Object -OutVariable +output1 @{ n = 'NSG Name'; e = {$obj.Name}},
+                    @{ n = 'Rule Name' ; e = {$_.Name} },
+                    @{ n = 'ResourceGroupName'; e = {$obj.ResourceGroupName}},
+                    @{ n = 'SourcePortRange';e = {$_.SourcePortRange}},
+                    @{ n = 'DestinationPortRange';e ={$_.DestinationPortRange}},
+                    @{ n = 'Access' ; expression = {$_.Access} },
+                    @{ n = 'Priority' ; expression = {$_.Priority} },
+                    @{ n = 'Direction'; e ={$_.Direction}} 
+            } 
+            $output1 | Export-Csv "$exportPath" -NoTypeInformation -Encoding ASCII
+
+    }
+    else{
+            foreach ($obj in $nsgs)
+            {
+                Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $obj | Select-Object -OutVariable +output1 @{ n = 'NSG Name'; e = {$obj.Name}},
+                    @{ n = 'Rule Name' ; e = {$_.Name} },
+                    @{ n = 'ResourceGroupName'; e = {$obj.ResourceGroupName}},
+                    @{ n = 'SourcePortRange';e = {$_.SourcePortRange}},
+                    @{ n = 'DestinationPortRange';e ={$_.DestinationPortRange}},
+                    @{ n = 'Access' ; expression = {$_.Access} },
+                    @{ n = 'Priority' ; expression = {$_.Priority} },
+                    @{ n = 'Direction'; e ={$_.Direction}} 
+            }#$output1 | Export-Csv "$exportPath" -NoTypeInformation -Encoding ASCII
+            $output1 | Export-Csv "$exportPath" -NoTypeInformation -Encoding ASCII
+    };
+
+
+    }
+}
+catch 
+{
+    Write-Host -ErrorAction Stop
+}
